4638d4a88ae0b175faeab574b2120211f0260be9
mullemi5 <mullemi5@fel.cvut.cz>
2019-05-09 14:33:26 +0200

    quit menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,6 +12,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml")); //also instantiates the associated controller
+        root.getStylesheets().add(getClass().getResource("MyStyle1.css").toExternalForm());
+        primaryStage.setTitle("Taskira");
+        primaryStage.setScene(new Scene(root, 400, 250));
+        primaryStage.show();
     }
 }
--- src/main/java/MainWindowController.java
+++ src/main/java/MainWindowController.java
@@ -1,2 +1,33 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Window;
+
+import java.util.Optional;
+
 public class MainWindowController {
+
+    public MainWindowController() {
+    }
+
+    //can also have initialize method, and also a private URL location attribute, and also ResourceBundle resources attribute
+
+    @FXML //so that even if its private, the loader can access it
+    private MenuItem fileQuit;
+
+
+    @FXML
+    protected void handleFileQuitMenuAction(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Quit");
+        alert.setHeaderText(null);
+        alert.setContentText("This will kill everything, are you sure?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            //TODO send message to model so that it properly closes
+            System.exit(0);
+        }
+    }
 }
